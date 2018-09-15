174eaab2b3385b3329d94427d9b792ec674cddf6
Skorob0g <skoro.toshka@gmail.com>
2018-09-15 21:36:17 +0300

    solution v1.0

---
+++ 4-functions/ida004.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+long double mcalc(double x, long double a2, long n){
+	return pow((-1), n) * pow(x, (2 * n + 1)) / (2 * n + 1) + a2;
+}
+
+long double my_arctg(double x, long double eps){
+	const long NMAX = 1000000000000;
+	long double a2 = x, a1;
+	for(long n = 1; n < NMAX; n++){
+		a1 = mcalc(x, a2, n);
+		if (abs(abs(a1) - abs(a2)) < eps)
+			return a1;
+		a2 = a1;
+	}
+	return a1; //вывод при недостаточной точности
+}
+
+int main(){
+	double x1, x2, dx;
+	long double a1, a2, eps;
+	const long NMAX = 1000000000000;
+	const string LINE = "----------------------------------------";
+	cout << "Write x1, x2, dx, eps ";
+	cin >> x1 >> x2 >> dx >> eps;
+	cout <<endl << LINE<< endl << "|"  << setw(13) << "X|" << setw(13) << "arctg(x)|" << setw(13) << "P-d arctg(x)|" << endl << LINE << endl;
+	int k;
+
+	for(double x = x1; x < x2; x += dx){ 
+		a1 = my_arctg(x, eps);
+
+		cout << "|" <<setw(12) << fixed << x << "|" << setw(12) << fixed <<  a1 << "|" << setw(12) << fixed << atan(x) << "|" << endl;
+	} 
+	cout << LINE << endl;
+	return 0;
+}

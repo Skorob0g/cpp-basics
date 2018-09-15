339f09867a05ea483d33698bb5eaf642fe8c0d08
Skorob0g <skoro.toshka@gmail.com>
2018-09-15 16:15:22 +0300

    variant 8 -> 9

--- 1-linear-program/ida001.cpp
+++ 1-linear-program/ida001.cpp
@@ -4,15 +4,15 @@
 using namespace std;
 
 int main(){
-	double x, y, z1, z2;
-	cout << "Write x ";
-	cin >> x;
-	cout << "Write y ";
-	cin >> y;
+	double a, b, z1, z2;
+	cout << "Write a ";
+	cin >> a;
+	cout << "Write b ";
+	cin >> b;
 
-	z1 = pow(cos(x), 4) + pow(sin(y), 2) + pow(sin(2*x), 2) * (1/4) - 1;
+	z1 = pow((cos(a) - cos(b)), 2) - pow((sin(a) - sin(b)), 2);
 
-	z2 = sin(y + x) * sin(y - x);
+	z2 = -4 * pow(sin((a - b) / 2), 2) * cos(a + b);
 	
 	cout << "z1 = " << z1 << endl << "z2 = " << z2 << endl;
 	return 0;

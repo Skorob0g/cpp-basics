3fef751716670d98ee9572cf775fed7efc8d21eb
Skorob0g <skoro.toshka@gmail.com>
2018-09-20 18:38:33 +0300

    solution v1.0

---
+++ 7-strings/ida007.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+	cout << "write file name ";
+	string filename, s;
+	cin >> filename;
+	ifstream fin(filename);
+	if (!fin.is_open()){
+		cout << "can't open file!" << endl;
+		return 1;
+	}
+
+	int n, k = 0;
+	cout << "write quantity of words ";
+	cin >> n;
+
+	bool fileEndFlag = false;
+	while(getline(fin, s, '.')){
+		for (int i = 1; i < s.length(); i++){
+			if (s[i] == ' ')
+				k++;
+		}
+
+		if (k == (n - 1))
+			cout << s << endl;
+		k = 0;
+	}
+
+	fin.close();
+	return 0;
+}

c59e80f53c5c832315949eae163cd81f2b6f8429
Skorob0g <skoro.toshka@gmail.com>
2018-09-23 02:46:55 +0300

    oop improved

--- 9-multi-file-project/myfunc.cpp
+++ 9-multi-file-project/myfunc.cpp
@@ -1,7 +1,52 @@
 #include "myheader.hpp"
-void badsort(double * a, int n){                                                                            
-    for (int j = 0; j < n; j++)                                                                             
-        for (int i = 1; i < n - j; i++)                                                                     
-            if ((fabs(a[i - 1]) < EPS) && (fabs(a[i]) > EPS))                                               
-                std::swap(a[i - 1], a[i]);                                                                       
+void badsort(double * a, int n){
+
+    for (int j = 0; j < n; j++)
+		for (int i = 1; i < n - j; i++)
+			if ((fabs(a[i - 1]) < EPS) && (fabs(a[i]) > EPS))
+				std::swap(a[i - 1], a[i]);
+}
+
+void absmax(double * a, int n){
+	double absmax = -1;
+	int iabsmax = 0;
+	for (int i = 0; i < n; i++){
+		if (fabs(a[i]) > absmax){
+			absmax = fabs(a[i]);
+			iabsmax = i;
+		}
+	}
+	cout << endl << "Abs max = a["<< iabsmax <<"] = " << a[iabsmax] << endl;
+}
+
+void sumbpos(double * a, int n){
+	bool pos1 = false, pos2 = false;
+	double sum = 0;
+	for (int i = 0; i < n; i++){
+		if (!pos1){
+			if (a[i] > EPS)
+				pos1 = true;
+		}
+		else
+			if (!pos2){
+				if (a[i] > EPS)
+					pos2 = true;
+				else
+					sum += a[i];
+			}
+	}
+	if (pos1 && pos2)
+		cout << "sum = " << sum << endl;
+	else
+		cout << "there are no 2 positive elements" << endl;
+}
+
+void printarr(double * a, int n){
+	for (int i = 0; i < n; i++)
+		cout << a[i] << " ";
+}
+
+void readarr(double * a, int n){
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
 }
--- 9-multi-file-project/myheader.hpp
+++ 9-multi-file-project/myheader.hpp
@@ -2,4 +2,14 @@
 #include <cmath>
 #define EPS 0.00000001
 
+using namespace std;
+
 void badsort(double * a, int n);
+
+void absmax(double * a, int n);
+
+void sumbpos(double * a, int n);
+
+void printarr(double * a, int n);
+
+void readarr(double * a, int n);

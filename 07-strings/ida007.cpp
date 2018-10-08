076bff9494f4d5e8bf083b26a08f316aacfc9803
Skorob0g <skoro.toshka@gmail.com>
2018-10-08 20:39:59 +0300

    naming changed

---
+++ 07-strings/ida007.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+	cout << "write file name ";
+	string filename, s;
+	cin >> filename;
+	ifstream fin(filename);
+	if (!fin.is_open()){
+		cout << "can't open file!" << endl;
+		return 1;
+	}
+
+	int n, k = 0;
+	cout << "write the number of words ";
+	cin >> n;
+
+	while(getline(fin, s, '.')){
+		for (unsigned int i = 1; i < s.length(); i++){
+			if (s[i] == ' ')
+				k++;
+		}
+
+		if (k == (n - 1))
+			cout << s << endl;
+		k = 0;
+	}
+
+	fin.close();
+	return 0;
+}
